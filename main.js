a055edd706531c69b2a777b34b92b5fbadf488f5
Josh <karta.josh.natin@gmail.com>
2024-02-11 20:46:40 -0300

    add popup message

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-onload = () => {
+// onload = () => {
+//   document.body.classList.remove("container");
+// };
+
+window.onload = function() {
+  document.getElementById('popup').style.display = 'block';
+};
+
+document.getElementById('closeButton').onclick = function() {
+  document.getElementById('popup').style.display = 'none';
   document.body.classList.remove("container");
 };
+
